a72dc5205e60d8f163a2f0ce815baf0173d6eb3a
carlajl14 <113455597+carlajl14@users.noreply.github.com>
2025-02-19 08:53:06 +0000

    proxy

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from PIL import Image
 from fpdf import FPDF
 import os
 
 app = Flask(__name__)
 
+# Define tu secreto de RapidAPI (asegúrate de mantener esto seguro)
+RAPIDAPI_SECRET = '5ec60b00-eddc-11ef-9c15-59f608f93374'
+
 @app.route('/convert', methods=['POST'])
 def convert_image_to_pdf():
+    # Verificar el encabezado de RapidAPI
+    rapidapi_secret = request.headers.get('X-RapidAPI-Proxy-Secret')
+    if rapidapi_secret != RAPIDAPI_SECRET:
+        return jsonify({'error': 'Acceso no autorizado'}), 403
+
     if 'image' not in request.files:
         return "No file part", 400
 
